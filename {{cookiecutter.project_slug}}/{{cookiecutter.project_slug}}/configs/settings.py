1a9a67f20cef7ccb8fbde22ac96fe64dc6250be3
Cuong Nguyen <cuongnb14@gmail.com>
2019-06-08 16:28:19 +0700

    add USER_AGENT config

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/configs/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/configs/settings.py
@@ -21,6 +21,7 @@ NEWSPIDER_MODULE = '{{cookiecutter.project_slug}}.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 # USER_AGENT = 'jokes (+http://www.yourdomain.com)'
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
